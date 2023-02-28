b1a6fcca15fa71310749961b0b29bf7f65e5d1cb
Aditya Gupta <adityag@gmail.com>
2023-02-28 18:10:22 -0500

    swag money

---
+++ Source/neonekon/neonekonGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "neonekonGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NEONEKON_API AneonekonGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
