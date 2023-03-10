929689eca26a02f8b54ee7f94d34b30c6bd9efac
Aditya Gupta <adityag@gmail.com>
2023-03-10 00:47:45 -0500

    fix #9, fix #13

---
+++ Source/neonekon/Util/BlueprintMathUtil.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BlueprintMathUtil.h"
+
+float UBlueprintMathUtil::smoothstep(float min, float max, float x)
+{
+    x = std::clamp((x - min) / (max - min), 0.0f, 1.0f);
+    return x * x * (3 - 2 * x);
+}
+
+float UBlueprintMathUtil::offsetSmoothstep(float x)
+{
+    return (smoothstep(-0.2, 0.2, x) + smoothstep(0.8, 1.2, x) - 0.5 + x / 5.0) / 1.2;
+}
